45265a9a0cc72172c43cba825a1fc009ffa4998d
Haining Pan <jackpan1994@gmail.com>
2019-12-05 12:00:09 -0500

    save banddiff

--- banddiff.m
+++ banddiff.m
@@ -2,23 +2,22 @@ function banddiff(NV,Npsi,w)
 Vrange=linspace(0,20,NV);
 psirange=linspace(0,360,Npsi);
 GammaMap=zeros(length(Vrange),length(psirange));
-kpMap=zeros(length(Vrange),length(psirange));
+% kpMap=zeros(length(Vrange),length(psirange));
 func=@(psi,V) energydiff(psi,V,w);
 
 parfor Vindex=1:NV
     for psiindex=1:Npsi
-        [GammaMap(Vindex,psiindex),kpMap(Vindex,psiindex)]...
-            =feval(func,psirange(psiindex),Vrange(Vindex));
+        GammaMap(Vindex,psiindex)=feval(func,psirange(psiindex),Vrange(Vindex));
     end
 end
-save(sprintf('banddiffNV%dNpsi%dw%d.mat',NV,Npsi,w));
+save(sprintf('banddiffNV%dNpsi%dw%d.dat',NV,Npsi,w),'GammaMap','-ascii');
 
 function [diffGamma,diffkp]=energydiff(psi,V,w)
 parameters=mainTMD('m',0.35,'psi',psi,'V',V,'w',w,'theta',3);
 val=energyTMD(0,0,parameters);
 diffGamma=val(1)-val(2);
-val=energyTMD(parameters.kp(1),parameters.kp(2),parameters);
-diffkp=val(1)-val(2);
+% val=energyTMD(parameters.kp(1),parameters.kp(2),parameters);
+% diffkp=val(1)-val(2);
 % val=energyTMD(parameters.kn(1),parameters.kn(2),parameters);
 % diffkn=val(1)-val(2);
 end
--- peakdiff.m
+++ peakdiff.m
@@ -1,10 +1,9 @@
 % load('dos.mat')
-[~,int1]=max(dos1,[],3);
-[~,int2]=max(dos2,[],3);
+[~,index1]=max(dos1,[],3);
+[~,index2]=max(dos2,[],3);
 
 for i=1:length(enlist1)
     for j=1:length(enlist1)
-        peakd(i,j)=enlist1(i,j,int1(i,j))-enlist2(i,j,int2(i,j));
+        peakd(i,j)=enlist1(i,j,index1(i,j))-enlist2(i,j,index2(i,j));
     end
 end
-% peakd=enlist1(int1)-enlist2(int2);
